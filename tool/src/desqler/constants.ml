42dacd041541ff1b620313eb16ec31ff9745dea3
Kiarahmani <rahmank@purdue.edu>
2018-07-25 23:32:09 -0400

    automation progress

--- tool/src/desqler/constants.ml
+++ tool/src/desqler/constants.ml
@@ -6,8 +6,10 @@ type c = SERIALIZABILITY | EVENTUAL_SERIALIZABILITY
 
 
 (*SET THE FOLLOWING CONSTANTS BEFORE RUNNING YOUR ANALYSIS*)
-let _CORRECTNESS = SERIALIZABILITY
-let _MAX_CYCLE_LENGTH = 14
-let _GUARANTEE = [(PSI,Some "Read",None);(SER,Some "Write1",None);(SER,Some "Write2",None)]
+let _CORRECTNESS = EVENTUAL_SERIALIZABILITY
+let _MAX_CYCLE_LENGTH = 3
+let _GUARANTEE = [(EC,Some "Read",None)
+                  (EC,Some "Write1",None)
+                  (EC,Some "Write2",None)]
 
 
