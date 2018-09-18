b37477a87664742e11127e523caf431ca98f36dd
Kiarahmani <rahmank@purdue.edu>
2018-09-18 15:54:04 -0400

    ready to add timing

--- tool/src/desqler/constants.ml
+++ tool/src/desqler/constants.ml
@@ -6,17 +6,7 @@ type c = SERIALIZABILITY | EVENTUAL_SERIALIZABILITY
 
 
 (*SET THE FOLLOWING CONSTANTS BEFORE RUNNING YOUR ANALYSIS*)
-let _CORRECTNESS = EVENTUAL_SERIALIZABILITY
-let _MAX_CYCLE_LENGTH = 4
-let _GUARANTEE = [
-  (SER,Some "Add_link",None); 
-  (SER,Some "Add_node",None); 
-  (EC,Some "Count_link",None); 
-  (SER,Some "Delete_link",None); 
-  (EC,Some "Delete_Node",None); 
-  (SER,Some "Get_link",None); 
-  (EC,Some "Get_link_list",None); 
-  (EC,Some "Get_node",None); 
-  (EC,Some "Update_link",None); 
-  (RC,Some "Update_node",None); 
-]
+let _CORRECTNESS = SERIALIZABILITY
+let _MAX_CYCLE_LENGTH = 3
+let _GUARANTEE = [(EC,Some "Write",None);
+                  (EC,Some "Read",None)]
